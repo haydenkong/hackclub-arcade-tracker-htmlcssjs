efd6490a914965893f2c78da9e0e9a002cb41a23
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-08 14:58:50 +1000

    reworked shop page

---
+++ max_order_quantity.js
@@ -0,0 +1,10 @@
+const max_order_quantity = {
+  "YubiKey": 5,
+  "Breadboard": 10,
+  "Soldering iron": 5,
+  "Pile of stickers": 10,
+  "Multimeter": 5,
+  "Logic Analyzer": 5,
+  "Domain for 1 year": 10,
+  "Sticker of your choice": 10
+};
